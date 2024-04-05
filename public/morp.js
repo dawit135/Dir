921a7f2dccaa07979f1829d7249b443c7f17c304
dawit135 <davotekie@gmail.com>
2024-04-05 20:20:23 +0300

    added a newer file

---
+++ public/morp.js
@@ -0,0 +1,12 @@
+const stuff = document.querySelector('.mainContent');
+
+
+const display = () => {
+   
+    console.log("main content clicked")
+   
+}
+
+
+
+stuff.addEventListener('click',display)
