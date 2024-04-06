15f6c47863b7ac93e312e86b2580990e2a60d46b
dawit135 <davotekie@gmail.com>
2024-04-06 23:26:33 +0300

    updated auth

--- public/morp.js
+++ public/morp.js
@@ -1,4 +1,8 @@
 const stuff = document.querySelector('.mainContent');
+const logger = document.querySelector('.loginInfo');
+const user = document.querySelector('#username');
+const passwr = document.querySelector('#password');
+
 
 
 const display = () => {
@@ -8,5 +12,14 @@ const display = () => {
 }
 
 
+const showInfo = () => {
+     
+     console.log(user.value)
+     console.log(passwr.value)
+     logger.reset()
+
+}
+
 
 stuff.addEventListener('click',display)
+logger.addEventListener('submit',showInfo)
